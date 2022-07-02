7e18c99186528c7760da559c5f759427447c27a8
Aleksandr F <a7214370@gmail.com>
2022-07-02 23:15:27 +0300

    Setup ViewController

--- Magic8Ball/ViewController.swift
+++ Magic8Ball/ViewController.swift
@@ -8,6 +8,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-    let ballArray = [#imageLiteral(resourceName: "ball1"), #imageLiteral(resourceName: "ball2"), #imageLiteral(resourceName: "ball3"), #imageLiteral(resourceName: "ball4"), #imageLiteral(resourceName: "ball5")]
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var askButton: UIButton!
+    
+    override func viewDidLoad() {
+            super.viewDidLoad()
+        askButton.layer.cornerRadius = 10
+        }
+    
+    @IBAction func askButtonPressed(_ sender: UIButton) {
+        let ballArray = [#imageLiteral(resourceName: "ball1"), #imageLiteral(resourceName: "ball2"), #imageLiteral(resourceName: "ball3"), #imageLiteral(resourceName: "ball4"), #imageLiteral(resourceName: "ball5")]
+    }
 }
 
